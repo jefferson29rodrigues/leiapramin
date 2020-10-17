f73c3e289e788520fe0a48e9ae96bd5c19d588f2
Jefferson Rodrigues <jeffersonrodrigues5030@gmail.com>
2020-10-17 02:03:32 -0300

    add mudar idioma e velocidade

--- javascript.js
+++ javascript.js
@@ -1,8 +1,52 @@
 const utterance = new SpeechSynthesisUtterance();
 
-utterance.lang = "pt-BR";
-utterance.rate = 0.75;
+let english = false;
 
+
+function mudarParaIngles() {
+    let english = true;
+
+    if (english) {
+        utterance.lang = "en-US";
+        utterance.rate = 0.50;
+
+    }  else {
+        utterance.lang = "pt-BR";
+        utterance.rate = 0.75;
+    }    
+
+}
+
+function mudarParaPortugues() {
+    let english = false;
+
+    if (english) {
+        utterance.lang = "en-US";
+        utterance.rate = 0.50;
+        
+    }  else {
+        utterance.lang = "pt-BR";
+        utterance.rate = 0.75;
+    }
+
+}
+
+
+function velo05 () {
+    utterance.rate = 0.5;
+}
+
+function velo1 () {
+    utterance.rate = 1;
+}
+
+function velo15 () {
+    utterance.rate = 1.5;
+}
+
+function velo2 () {
+    utterance.rate = 2;
+}
 
 
 function speak() {
